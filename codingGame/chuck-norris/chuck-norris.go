d93805aba446d57054a18e434197f887c7d57ee4
Christophe PRIGENT <christopheprigent@PSD12823.local>
2017-11-07 12:20:54 +0100

    codingGame chuck-norris go level up

--- codingGame/chuck-norris/chuck-norris.go
+++ codingGame/chuck-norris/chuck-norris.go
@@ -9,24 +9,24 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Buffer(make([]byte, 1000000), 1000000)
-
 	scanner.Scan()
 	msg := scanner.Text()
 	bin := ""
-	for i := 0; i < len(msg); i++ {
-		var ord = rune(string(msg[i])[0])
-		bin += fmt.Sprintf("%b", ord)
-	}
 
+	for _, char := range msg {
+		bin += fmt.Sprintf("%07b", char)
+	}
 	fmt.Println(Bin2Chuck(bin)) // Write answer to stdout
 }
 
-// Bin2Chuck helper for chuck
+// Bin2Chuck : Chuck Norris' keyboard has 2 keys: 0 and white space.
 func Bin2Chuck(bin string) (chuck string) {
-	// fmt.Println("Bin2Chuck:", bin)
 	chuck = ""
 	for i := 0; i < len(bin); i++ {
 		c := string(bin[i])
+		if i > 0 {
+			chuck += " "
+		}
 		if c == "0" {
 			chuck += "00 0"
 		} else {
@@ -36,7 +36,7 @@ func Bin2Chuck(bin string) (chuck string) {
 			chuck += "0"
 			i++
 		}
-		chuck += " "
+
 	}
 	return
 }
